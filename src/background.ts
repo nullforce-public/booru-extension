831ee677603ea52fd9a8e35e9cea06d37056b777
nullforce <glenngit@nullforce.com>
2024-11-02 16:02:01 -0500

    Add GitHub action for CI

--- src/background.ts
+++ src/background.ts
@@ -12,13 +12,9 @@ chrome.runtime.onMessage.addListener(async function (request: IBackgroundMessage
     // Get settings
     const settings = await getSettings();
 
-    if (request.action === "addToEagleFromUrl") {
-        addToEagleFromUrl(request);
-    }
-    else if (request.action === "getEagle") {
-        console.log("nfDerpi: BG getEagle");
-        const url = `http://localhost:41595/api/application/info?token=${settings.eagleApiToken}`
-        fetch(url).then(response => response.json());
+    switch (request.action) {
+        case "addToEagleFromUrl": addToEagleFromUrl(request); break;
+        case "getEagle": getEagle(settings.eagleApiToken); break;
     }
 });
 
@@ -32,4 +28,14 @@ function addToEagleFromUrl(request: any): void {
         body: request.data
     }).then(response => console.log(response))
         .catch(error => console.log("nfDerpi Error: ", error));
+}
+
+/**
+ * Readies the Eagle API for use. Calling the info endpoint will warm up the API.
+ * @param eagleApiToken The Eagle API token to use.
+ */
+function getEagle(eagleApiToken: string): void {
+    console.log("nfDerpi: BG getEagle");
+    const url = `http://localhost:41595/api/application/info?token=${eagleApiToken}`
+    fetch(url).then(response => response.json());
 }
